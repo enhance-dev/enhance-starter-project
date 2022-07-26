6e2395e2ab30d3f48ed841baeb2502cc375c5f98
macdonst <simon.macdonald@gmail.com>
2022-07-25 20:35:30 -0400

    Create manifest file
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

---
+++ scripts/createManifest.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+const { resolve } = require('path')
+const { lstatSync, readdirSync, writeFileSync } = require('fs')
+const cwd = process.cwd()
+
+function readDir(directory) {
+    let files = []
+    readdirSync(directory).forEach(file => {
+        let current = resolve(directory, file)
+        if (lstatSync(current).isDirectory()) {
+            files.push(...readDir(current))
+        } else {
+            files.push(current.replace(cwd, '@enhance/starter-project'))
+        }
+    });
+    return files
+}
+
+let manifest = {
+    fileList: []
+}
+
+manifest.fileList.push(...readDir('./app'))
+manifest.fileList.push(...readDir('./public'))
+
+writeFileSync('./manifest.json', JSON.stringify(manifest))
