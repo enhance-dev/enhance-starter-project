be3938f27ae6d652d08d8769d9e75ce5d1814350
Cole Peters <cole@colepeters.com>
2024-02-27 12:55:23 -0600

    Include styles as <link> for local, <style> for deploy

--- app/head.mjs
+++ app/head.mjs
@@ -1,11 +1,17 @@
 import { getStyles } from '@enhance/arc-plugin-styles'
 
-const { linkTag } = getStyles
-
 export default function Head(state) {
   const { store = {} } = state
+
   // pageTitle is set in /app/preflight.mjs
   const { pageTitle = '' } = store
+
+  // Enhance Styles
+  // CSS will be included as a <link> tag for local development.
+  // For deployments, CSS will be included as a <style> tag in order to eliminate the blocking network request created by <link> tags.
+  const styles = process.env.ARC_LOCAL
+    ? getStyles.linkTag()
+    : getStyles.styleTag()
 
   return `
     <!DOCTYPE html>
@@ -14,7 +20,7 @@ export default function Head(state) {
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <title>${pageTitle}</title>
-      ${linkTag()}
+      ${styles}
       <link rel="icon" href="/_public/favicon.svg">
       <meta name="description" content="The HTML first full stack web framework.">
     </head>
